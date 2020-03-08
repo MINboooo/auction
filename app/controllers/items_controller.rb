a7ff670d8f7d727506587a2b2a486fb8625cf463
MINboooo <minminnotsemi1203@hotmail.co.jp>
2020-03-08 15:57:40 +0900

    add Validation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,9 +15,11 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    @item.save
-
-    redirect_to @item
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
   end
 
   def edit
